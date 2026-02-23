c014f3a3136664ec8f1b9863b52398eb77d6996e
Zach <1260805505c@gmail.com>
2026-02-23 17:56:24 +0700

    Implement face detection

---
+++ src/cv_pkg/src/cv_face_detect.cpp
@@ -0,0 +1,59 @@
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/region_of_interest.hpp>
+
+const std::string NODE_NAME = "cv_face_detect_node";
+const std::string CAM_TOPIC_NAME = "/kinect2/qhd/image_raw";
+const std::string FRAME_TOPIC_NAME = "/face_detector_input";
+const std::string FACE_TOPIC_NAME = "/face_position";
+const std::string FACE_WIN_NAME = "Face";
+const int QOS1 = 1;
+
+static std::shared_ptr<rclcpp::Node> node;
+static rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr frame_pub;
+static cv::Mat imgFace;
+
+void CamRGBCallback(sensor_msgs::msg::Image::ConstSharedPtr msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+    imgFace = cv_ptr->image;
+
+    frame_pub->publish(*msg);
+}
+
+void FacePoseCallback(sensor_msgs::msg::RegionOfInterest::ConstSharedPtr msg)
+{
+    cv::rectangle(imgFace, 
+        cv::Point(msg->x_offset, msg->y_offset),
+        cv::Point(msg->x_offset + msg->width, msg->y_offset + msg->height), 
+        cv::Scalar(0, 0, 255),
+        2, 
+        cv::LINE_8);
+    cv::imshow(FACE_WIN_NAME, imgFace);
+    cv::waitKey(1);
+    
+}
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+
+    node = std::make_shared<rclcpp::Node>(NODE_NAME);
+
+    auto rgb_sub = node->create_subscription<sensor_msgs::msg::Image>(CAM_TOPIC_NAME, QOS1, CamRGBCallback);
+    frame_pub = node->create_publisher<sensor_msgs::msg::Image>(FRAME_TOPIC_NAME, QOS1);
+    auto face_sub =
+        node->create_subscription<sensor_msgs::msg::RegionOfInterest>(FACE_TOPIC_NAME, QOS1, FacePoseCallback);
+
+    cv::namedWindow(FACE_WIN_NAME);
+
+    rclcpp::spin(node);
+
+    cv::destroyAllWindows();
+    rclcpp::shutdown();
+}
--- src/wpr_simulation2-main/scripts/install_dep_face.sh
+++ src/wpr_simulation2-main/scripts/install_dep_face.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yes | pip3 install face_recognition -i https://pypi.tuna.tsinghua.edu.cn/simple
+yes | pip3 install face_recognition -i https://pypi.tuna.tsinghua.edu.cn/simple # 你可以直接使用官方的源
