006d6e1c56e84dd95a0647c3b7d5d11ba5c5336d
Zach <1260805505c@gmail.com>
2026-02-23 19:53:10 +0700

    Implement visual servo

--- src/cv_pkg/src/cv_face_detect.cpp
+++ src/cv_pkg/src/cv_face_detect.cpp
@@ -4,17 +4,47 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/region_of_interest.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 
 const std::string NODE_NAME = "cv_face_detect_node";
 const std::string CAM_TOPIC_NAME = "/kinect2/qhd/image_raw";
 const std::string FRAME_TOPIC_NAME = "/face_detector_input";
 const std::string FACE_TOPIC_NAME = "/face_position";
+const std::string VEL_TOPIC_NAME = "/cmd_vel";
 const std::string FACE_WIN_NAME = "Face";
 const int QOS1 = 1;
+const int QOS10 = 10;
 
 static std::shared_ptr<rclcpp::Node> node;
 static rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr frame_pub;
+static rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub;
 static cv::Mat imgFace;
+static geometry_msgs::msg::Twist vel_cmd;
+static double Kp = 0.002;
+
+void DrawRectangle(sensor_msgs::msg::RegionOfInterest::ConstSharedPtr msg)
+{
+    cv::rectangle(imgFace, cv::Point(msg->x_offset, msg->y_offset),
+                  cv::Point(msg->x_offset + msg->width, msg->y_offset + msg->height), cv::Scalar(0, 0, 255), 2,
+                  cv::LINE_8);
+    cv::imshow(FACE_WIN_NAME, imgFace);
+    cv::waitKey(1);
+}
+
+void Follow(sensor_msgs::msg::RegionOfInterest::ConstSharedPtr msg)
+{
+    int img_center = imgFace.cols / 2;
+    int face_center = msg->x_offset + msg->width / 2;
+    int error = face_center - img_center;
+
+    if (abs(error) < 20) {
+        vel_cmd.angular.z = 0.0;
+    }
+
+    vel_cmd.angular.z = -Kp * error;
+
+    vel_pub->publish(vel_cmd);
+}
 
 void CamRGBCallback(sensor_msgs::msg::Image::ConstSharedPtr msg)
 {
@@ -28,15 +58,9 @@ void CamRGBCallback(sensor_msgs::msg::Image::ConstSharedPtr msg)
 
 void FacePoseCallback(sensor_msgs::msg::RegionOfInterest::ConstSharedPtr msg)
 {
-    cv::rectangle(imgFace, 
-        cv::Point(msg->x_offset, msg->y_offset),
-        cv::Point(msg->x_offset + msg->width, msg->y_offset + msg->height), 
-        cv::Scalar(0, 0, 255),
-        2, 
-        cv::LINE_8);
-    cv::imshow(FACE_WIN_NAME, imgFace);
-    cv::waitKey(1);
-    
+    DrawRectangle(msg);
+
+    Follow(msg);
 }
 
 int main(int argc, char* argv[])
@@ -47,6 +71,7 @@ int main(int argc, char* argv[])
 
     auto rgb_sub = node->create_subscription<sensor_msgs::msg::Image>(CAM_TOPIC_NAME, QOS1, CamRGBCallback);
     frame_pub = node->create_publisher<sensor_msgs::msg::Image>(FRAME_TOPIC_NAME, QOS1);
+    vel_pub = node->create_publisher<geometry_msgs::msg::Twist>(VEL_TOPIC_NAME, QOS10);
     auto face_sub =
         node->create_subscription<sensor_msgs::msg::RegionOfInterest>(FACE_TOPIC_NAME, QOS1, FacePoseCallback);
 
